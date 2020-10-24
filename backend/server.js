ef6a8e4e7452ec35ce03f0ec52aae4afade05bbc
viveloper <ucsik1234@gmail.com>
2020-10-24 15:26:06 +0900

    Connecting to Database with Mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const connectDB = require('./config/db');
+
+// Load env vars
+dotenv.config({ path: 'backend/config/config.env' });
+
+// Connect to database
+connectDB();
 
 // Router files
 const bootcampsRouter = require('./routes/bootcamps');
-
-// Load env vars
-dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
