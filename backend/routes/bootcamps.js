62b620773a320a218dd78621b723495e728314c9
viveloper <ucsik1234@gmail.com>
2020-10-27 16:39:52 +0900

    Auth Protect Middleware

--- backend/routes/bootcamps.js
+++ backend/routes/bootcamps.js
@@ -16,19 +16,21 @@ const coursesRouter = require('./courses');
 
 const router = express.Router();
 
+const { protect } = require('../middleware/auth');
+
 // re-route into other resource routers
 router.use('/:bootcampId/courses', coursesRouter);
 
 router
   .route('/')
   .get(advancedResults(Bootcamp, 'courses'), getBootcamps)
-  .post(createBootcamp);
+  .post(protect, createBootcamp);
 router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
-router.route('/:id/photo').put(bootcampPhotoUpload);
+router.route('/:id/photo').put(protect, bootcampPhotoUpload);
 router
   .route('/:id')
   .get(getBootcamp)
-  .put(updateBootcamp)
-  .delete(deleteBootcamp);
+  .put(protect, updateBootcamp)
+  .delete(protect, deleteBootcamp);
 
 module.exports = router;
