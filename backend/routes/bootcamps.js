f75cebc439f37ef492811cf87488a0532823b527
viveloper <ucsik1234@gmail.com>
2020-10-27 16:51:10 +0900

    Role Authorization

--- backend/routes/bootcamps.js
+++ backend/routes/bootcamps.js
@@ -16,7 +16,7 @@ const coursesRouter = require('./courses');
 
 const router = express.Router();
 
-const { protect } = require('../middleware/auth');
+const { protect, authorize } = require('../middleware/auth');
 
 // re-route into other resource routers
 router.use('/:bootcampId/courses', coursesRouter);
@@ -24,13 +24,15 @@ router.use('/:bootcampId/courses', coursesRouter);
 router
   .route('/')
   .get(advancedResults(Bootcamp, 'courses'), getBootcamps)
-  .post(protect, createBootcamp);
+  .post(protect, authorize('publisher', 'admin'), createBootcamp);
 router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
-router.route('/:id/photo').put(protect, bootcampPhotoUpload);
+router
+  .route('/:id/photo')
+  .put(protect, authorize('publisher', 'admin'), bootcampPhotoUpload);
 router
   .route('/:id')
   .get(getBootcamp)
-  .put(protect, updateBootcamp)
-  .delete(protect, deleteBootcamp);
+  .put(protect, authorize('publisher', 'admin'), updateBootcamp)
+  .delete(protect, authorize('publisher', 'admin'), deleteBootcamp);
 
 module.exports = router;
